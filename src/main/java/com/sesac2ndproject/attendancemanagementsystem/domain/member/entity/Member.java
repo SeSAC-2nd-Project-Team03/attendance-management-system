4c352bb3b48be9fb599deef5d81571e5b3e95956
soy <teabag759@gmail.com>
2025-11-28 13:14:02 +0900

    #1/feat: Member entity 대략적 구조 구축

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/entity/Member.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/entity/Member.java
@@ -1,4 +1,28 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.member.entity;
 
-public class Member {
+import com.sesac2ndproject.attendancemanagementsystem.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    private String password; // 암호화 저장 필수
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String address;
+    
 }
