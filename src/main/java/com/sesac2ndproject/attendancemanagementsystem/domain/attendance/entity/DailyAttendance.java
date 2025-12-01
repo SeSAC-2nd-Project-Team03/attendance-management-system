f9adc9019d8e05518a7e554780246aefd3ec2608
soy <teabag759@gmail.com>
2025-12-01 11:22:18 +0900

    impl: swagger 설정 완료

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/entity/DailyAttendance.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/entity/DailyAttendance.java
@@ -1,6 +1,7 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.attendance.entity;
 
-import com.sesac2ndproject.attendancemanagementsystem.global.type.AttendanceType;
+import com.sesac2ndproject.attendancemanagementsystem.global.entity.BaseTimeEntity;
+import com.sesac2ndproject.attendancemanagementsystem.global.type.AttendanceStatus;
 import jakarta.persistence.*; // ⭐️ 중요: javax가 아니라 jakarta여야 함
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +11,17 @@ import java.time.LocalDate;
 @Entity // 👈 이 녀석이 범인일 확률 99%
 @Getter
 @NoArgsConstructor
-public class DailyAttendance {
+public class DailyAttendance extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private Long member_id;
+    private Long memberId;
 
     private LocalDate date;
 
     @Enumerated(EnumType.STRING)
-    private AttendanceType status;
+    private AttendanceStatus status;
 }
