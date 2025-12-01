f37e8f269aabcd65bef3b5c04fda769b8043c6f9
soy <teabag759@gmail.com>
2025-12-01 10:50:29 +0900

    fix: Swagger 설정 수정 및 DailyAttendance 엔티티 추가

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/entity/DailyAttendance.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/entity/DailyAttendance.java
@@ -1,4 +1,26 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.attendance.entity;
 
+import com.sesac2ndproject.attendancemanagementsystem.global.type.AttendanceType;
+import jakarta.persistence.*; // ⭐️ 중요: javax가 아니라 jakarta여야 함
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity // 👈 이 녀석이 범인일 확률 99%
+@Getter
+@NoArgsConstructor
 public class DailyAttendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long member_id;
+
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    private AttendanceType status;
 }
--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/global/Config/SecurityConfig.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/global/Config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.sesac2ndproject.attendancemanagementsystem.global.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+//                .anyRequest().authenticated()
+//            );
+//
+//        return http.build();
+//    }
 }
