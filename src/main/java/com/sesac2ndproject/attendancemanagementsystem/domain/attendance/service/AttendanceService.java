d35f24e0d138dae8475f4a8986c10ed7b3c75cb3
soy <teabag759@gmail.com>
2025-12-01 16:54:40 +0900

    #4/feat: JwtTokenProvider 구현

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/service/AttendanceService.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/attendance/service/AttendanceService.java
@@ -1,4 +1,15 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.attendance.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AttendanceService {
+
+    // 회원 찾기
+
+    // 비밀번호 확인
+
+    // 인증 성공 -> 토큰 생성
 }
--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/global/jwt/JwtTokenProvider.java
@@ -1,4 +1,77 @@
 package com.sesac2ndproject.attendancemanagementsystem.global.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
 public class JwtTokenProvider {
+    private final Key key;
+    private final long accessTokenValidityInMilliseconds;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey,
+                            @Value("${jwt.expiration}") long accessTokenValidityInMilliseconds) {
+
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.accessTokenValidityInMilliseconds = accessTokenValidityInMilliseconds;
+    }
+
+    // 토큰 생성
+    public String createToken(String loginId, String role) {
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.accessTokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(loginId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 토큰에서 id 꺼내기
+    public String getLoginId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getSubject();
+
+    }
+
+    // 토큰 유효성 검사
+    public boolean validateToken(String token) {
+        try {
+            Jwts
+                    .parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJwt(token);
+
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+
+        return false;
+    }
+
 }
