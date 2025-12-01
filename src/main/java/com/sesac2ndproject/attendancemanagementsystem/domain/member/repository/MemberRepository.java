a37957e3d2b34702dd4e77e81254255d63aeaf33
soy <teabag759@gmail.com>
2025-12-01 14:51:59 +0900

    feat: MemberRepository 구현

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/repository/MemberRepository.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.member.repository;
 
-public class MemberRepository {
+import com.sesac2ndproject.attendancemanagementsystem.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 로그인 id로 회원 정보 찾기
+    Optional<Member> findByLoginId(String loginId);
+
+    // 중복 id 방지
+    boolean existsByLoginId(String loginId);
 }
