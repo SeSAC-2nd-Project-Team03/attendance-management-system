787af470fd980bbcf454b140a1f1623238fb3922
soy <teabag759@gmail.com>
2025-12-03 10:52:48 +0900

    feat: 관리자 회원 생성 비즈니스 로직 구현

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/service/MemberService.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/member/service/MemberService.java
@@ -1,4 +1,45 @@
 package com.sesac2ndproject.attendancemanagementsystem.domain.member.service;
 
+import com.sesac2ndproject.attendancemanagementsystem.domain.member.dto.MemberCreateRequest;
+import com.sesac2ndproject.attendancemanagementsystem.domain.member.entity.Member;
+import com.sesac2ndproject.attendancemanagementsystem.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
+
+    // 중복된 아이디가 있으면 안됨
+    // 비밀번호는 절대 평문으로 저장하면 안됨
+    // 관리자와 학생을 구분해서 저장해야 함
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long createMember(MemberCreateRequest request) {
+        // 중복 검사
+        if (memberRepository.existsByLoginId(request.getLoginId())) {
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다" + request.getLoginId());
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        // 엔티티 생성
+        Member member = Member.builder()
+                .loginId(request.getLoginId())
+                .password(request.getPassword())
+                .name(request.getName())
+                .phoneNumber(request.getPhoneNumber())
+                .role(request.getRole())
+                .build();
+
+        Member savedMember = memberRepository.save(member);
+        return savedMember.getId();
+    }
 }
