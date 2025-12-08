6cc5c47a9b1f6a025d7c07c270b2f3430edb1416
soy <teabag759@gmail.com>
2025-12-08 09:34:08 +0900

    #38/feat: apiresponse 구현

--- src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/auth/controller/AuthController.java
+++ src/main/java/com/sesac2ndproject/attendancemanagementsystem/domain/auth/controller/AuthController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @Tag(name = "Auth", description = "로그인 및 회원가입 인증 API")
 @RestController
